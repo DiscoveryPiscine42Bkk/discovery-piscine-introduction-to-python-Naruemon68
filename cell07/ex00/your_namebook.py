e5bfe101ddd122963175d5fbec4b81636bacec05
Naruemon68 <naruemonthongphaichit@gmail.com>
2025-06-19 04:33:37 +0000

    sgy

---
+++ cell07/ex00/your_namebook.py
@@ -0,0 +1,15 @@
+def array_of_names(name_dict):
+    full_names = []
+    for first, last in name_dict. items ():
+        full_name = f"{first.capitalize()} {last.capitalize()}"
+        full_names. append(full_name)
+    return full_names
+
+persons = {
+    "naruemon": "thongphaichit",
+    "yadaporn": "rattanakunchorn",
+    "aonaan": "music",
+    "music": "aonaan"
+}
+
+print(array_of_names(persons))
